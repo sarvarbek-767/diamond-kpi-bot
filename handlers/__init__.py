a584d12cf14fe2db054b7a358e88969960907f9f
sarvarbek-767 <sarvarbek898@gmail.com>
2026-07-28 10:47:20 +0500

    Add files via upload

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,3 +1,7 @@
-git add .
-git commit -m "Update handlers init file"
-git push origin main
+from .admin import router as admin_router
+from .callbacks import router as callbacks_router
+from .employee import router as employee_router
+from .kpi import router as kpi_router
+from .reminders import router as reminders_router
+from .reports import router as reports_router
+from .start import router as start_router
